8e577c8eb48c1e59b7228ffed12a2fc91b277a1d
SolalVall <solal.vallee@laposte.net>
2018-08-26 17:32:41 +0200

    Oops forgot to add ux script

---
+++ scripts/ux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Little alias for my pre-config tmux
+
+vim_tips=$HOME/.vimTips
+#if a command argument is defined
+if [ ! -z "$1" ];
+then
+	tmux new-session \; \
+		split-window -h \; \
+		send-keys "$1" C-m \; \
+		split-window -v -p 35 \; \
+		send-keys "cat $vim_tips" C-m \; 
+else
+	tmux new-session \; \
+		split-window -h \; \
+		send-keys 'ls -la' C-m \; \
+		split-window -v -p 35 \; \
+		send-keys "cat $vim_tips" C-m \; 
+fi
