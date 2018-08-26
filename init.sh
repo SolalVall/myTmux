bd3db6f35bbf98c491eff7b0518a438e508c970b
SolalVall <solal.vallee@laposte.net>
2018-08-26 16:31:24 +0200

    add a init script who make install and add tmux plugins

---
+++ init.sh
@@ -0,0 +1,22 @@
+###### INIT SCRIPT FOR TMUX PERSO CONFIG AND TPM (Tmux Plugin Manager) ########
+### FOR THE MOMENT DEBIAN BASED OS ONLY
+
+#Tmux useful variables
+tmux_folder="$HOME/.tmux"
+tpm="$tmux_folder/plugins/tpm"
+
+#Check if TMUX already installed
+if [ -z $(which tmux) ];
+then
+	sudo apt-get install tmux -y
+fi
+
+#check if tpm already installed
+if [ ! -d $tpm ];
+then
+	git clone https://github.com/tmux-plugins/tpm $tpm
+fi
+
+#Copy tmux conf and run a first tmux command who ask you to enter a specific combinaison of keys for installing tmux plugins
+cp .tmux.conf ~/.tmux.conf
+tmux new-session \;  send-keys 'clear && for i in  {1..25}; do echo ; done  && echo please enter Ctrl+b and I Shift+i && for i in  {1..25}; do echo ; done' c-m \;
